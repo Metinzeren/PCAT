d535b7d8fe1f2ce2a0277373d4e2483d25498a20
Metinzeren <zeren0760@hotmail.com>
2023-01-12 18:11:37 +0300

    pcat

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const ejs = require('ejs');
+const Photo = require('./models/Photo');
 const app = express();
+const fileUpload = require('express-fileupload');
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+//ROUTES
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
+  });
 });
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log('sunucu başlatıldı');
