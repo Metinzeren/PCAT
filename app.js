c9ef3005e239488674c7e2bc7c8d6a5cd88a5122
Metinzeren <zeren0760@hotmail.com>
2023-01-13 14:16:08 +0300

    lastCommit

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const Photo = require('./models/Photo');
 const app = express();
 const fileUpload = require('express-fileupload');
 mongoose.connect('mongodb://localhost/pcat-test-db');
-
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
@@ -17,41 +18,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
 //ROUTES
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos,
-  });
-});
-app.get('/photos/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
 
-app.post('/photos', async (req, res) => {
-  const uploadDir = 'public/uploads';
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-  let uploadImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
-
-  uploadImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadImage.name,
-    });
-    res.redirect('/');
-  });
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
 
 const port = 3000;
 app.listen(port, () => {
---
+++ controllers/photoController.js
@@ -0,0 +1,32 @@
+const Photo = require('../models/Photo');
+
+exports.getAllPhotos = async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
+  });
+};
+
+exports.getPhoto = async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+};
+
+exports.createPhoto = async (req, res) => {
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
+};
